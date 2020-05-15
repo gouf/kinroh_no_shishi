eb3d964f0de56c0784a0092e7205c2453ce27355
gouf <innocent.zero@gmail.com>
2020-05-15 15:41:16 +0900

    申請処理とスクリーンショット機能の追加

--- main.rb
+++ main.rb
@@ -1,31 +1,55 @@
+require 'date'
 require 'watir'
 require 'dotenv'
+require 'active_support/core_ext'
 require_relative 'lib/kinroh_no_shishi/site'
 require_relative 'lib/kinroh_no_shishi/login_page'
 require_relative 'lib/kinroh_no_shishi/worktime_page'
+require_relative 'lib/kinroh_no_shishi/screenshot'
 
-Dotenv.load
+module Task
+  # 指定した日付に対して、テンプレートに設定した値を自動入力・申請 (確定処理) をする
+  def self.run
+    Dotenv.load
 
-browser = Watir::Browser.new
-# browser.window.resize_to(2_500, 2_500) # width, height
+    Watir.default_timeout = 2.minutes # テザリングなどで遅い回線からアクセスした場合でも自動処理できるようにする
+    browser = Watir::Browser.new
 
-login_page =
-  KinrohNoShishi::LoginPage.new(browser)
+    # スクリーンショット作成用に充分なウィンドウサイズを設定
+    browser.window.resize_to(2_300, 900) # width, height
 
-login_page.open
+    login_page =
+      KinrohNoShishi::LoginPage.new(browser)
 
-worktime_page =
-  login_page.login_as(
-    corporation_code: ENV['KINROH_NO_SHISHI_HOJIN_CODE'],
-    user_id:          ENV['KINROH_NO_SHISHI_USER_ID'],
-    password:         ENV['KINROH_NO_SHISHI_PASSWORD']
-  )
+    login_page.open
 
-worktime_page.fill_form(
-  current_months_day: 31, # 今月n日を自動入力の対象にする
-  worktime_template_name: :work_at_home
-)
+    worktime_page =
+      login_page.login_as(
+        corporation_code: ENV['KINROH_NO_SHISHI_HOJIN_CODE'],
+        user_id:          ENV['KINROH_NO_SHISHI_USER_ID'],
+        password:         ENV['KINROH_NO_SHISHI_PASSWORD']
+      )
 
-worktime_page.save # 申請ボタンを押す
+    day_today = Date.today.day
 
-worktime_page.close
+    worktime_page.fill_form(
+      current_months_day: day_today, # 今月n日を自動入力の対象にする
+      worktime_template_name: :work_at_home
+    )
+
+    worktime_page.save # 申請ボタンを押す
+
+    screenshot_page =
+      KinrohNoShishi::Screenshot.new(worktime_page.browser)
+    screenshot_page.day_index = day_today # FIXME: 値の直接書き換えではなくメソッドの引数として値を渡す
+    screenshot_page.personal_application_list
+
+    worktime_page.close
+
+    # 撮影したスクリーンショットを Mac 環境の open コマンドで開く
+    system('open ~/kinroh_no_shishi/screenshot.png')
+    screenshot_page.close
+  end
+end
+
+Task.run
