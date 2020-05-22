3f08c0c70372cbdee253c3c729dc2766fd7b2f17
gouf <innocent.zero@gmail.com>
2020-05-22 18:22:50 +0900

    日付けが異なる場合の繰り返し処理に対応
    インスタンス化で状態を持たせて、日付けが異なる場合の対応

--- main.rb
+++ main.rb
@@ -7,9 +7,11 @@ require_relative 'lib/kinroh_no_shishi/login_page'
 require_relative 'lib/kinroh_no_shishi/worktime_page'
 require_relative 'lib/kinroh_no_shishi/screenshot'
 
-module Task
-  # 指定した日付に対して、テンプレートに設定した値を自動入力・申請 (確定処理) をする
-  def self.run
+# Watir の操作をラップしたクラスに対する実際の操作を提供する
+class KinrohNoShishiApplicationTask
+  attr_reader :worktime_page
+
+  def initialize
     Dotenv.load
 
     Watir.default_timeout = 2.minutes # テザリングなどで遅い回線からアクセスした場合でも自動処理できるようにする
@@ -23,33 +25,49 @@ module Task
 
     login_page.open
 
-    worktime_page =
+    @worktime_page =
       login_page.login_as(
         corporation_code: ENV['KINROH_NO_SHISHI_HOJIN_CODE'],
         user_id:          ENV['KINROH_NO_SHISHI_USER_ID'],
         password:         ENV['KINROH_NO_SHISHI_PASSWORD']
       )
+  end
 
-    day_today = Date.today.day
+  # 指定した日付に対して、テンプレートに設定した値を自動入力・申請 (確定処理) をする
+  def fill_form(day:, worktime_template_name:)
+      worktime_page.fill_form(
+        current_months_day: day, # 今月n日を自動入力の対象にする
+        worktime_template_name: worktime_template_name
+      )
 
-    worktime_page.fill_form(
-      current_months_day: day_today, # 今月n日を自動入力の対象にする
-      worktime_template_name: :work_at_home
-    )
+      worktime_page.save # 申請ボタンを押す
+  end
 
-    worktime_page.save # 申請ボタンを押す
+  def fill_forms(day_range:, worktime_template_name:)
+    day_range.each do |day|
+      worktime_page.fill_form(
+        current_months_day: day, # 今月n日を自動入力の対象にする
+        worktime_template_name: worktime_template_name
+      )
 
+      worktime_page.save # 申請ボタンを押す
+    end
+  end
+
+  def take_screenshot
     screenshot_page =
       KinrohNoShishi::Screenshot.new(worktime_page.browser)
-    screenshot_page.day_index = day_today # FIXME: 値の直接書き換えではなくメソッドの引数として値を渡す
-    screenshot_page.personal_application_list
 
-    worktime_page.close
+    screenshot_page.day_index = Date.today # FIXME: 値の直接書き換えではなくメソッドの引数として値を渡す
+    screenshot_page.personal_application_list
 
     # 撮影したスクリーンショットを Mac 環境の open コマンドで開く
     system('open ~/kinroh_no_shishi/screenshot.png')
-    screenshot_page.close
   end
 end
 
-Task.run
+task = KinrohNoShishiApplicationTask.new
+
+# task.fill_forms(day_range: (23..25), worktime_template_name: :work_at_home)
+task.fill_form(day: Date.today, worktime_template_name: :tokyo_office)
+task.take_screenshot
